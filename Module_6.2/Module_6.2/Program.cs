6b7974b8d739b970ff188a745c03dcde23ebef1a
Ilya <ilyakhodakov1996@mail.ru>
2022-11-20 09:56:43 +0300

    Добавьте файлы проекта.

---
+++ Module_6.2/Module_6.2/Program.cs
@@ -0,0 +1,95 @@
+﻿using System.Security.Principal;
+
+namespace Module_6._2
+{
+
+
+    class Pen
+    {
+        //Поля класса
+        public string color;
+        public int cost;
+
+        //Конструктор 1
+        public Pen()
+        {
+            color = "Черный";
+            cost = 100; 
+        }
+
+        //Конструктор 2
+        public Pen(string penColor, int penCost)
+        {
+            color=penColor;
+            cost = penCost;
+        }
+    }
+
+    //класс прямоугольник
+    class Rectangle
+    {
+        //стороны
+        public int a;
+        public int b;
+
+        //метод обозначающий расчет площади
+        public int Square()
+        {
+            return a * b;
+        }
+
+        //конструктор 1
+        public Rectangle()
+        {
+            a = 6;
+            b = 4 ;
+        }
+
+        //конструктор 2
+        public Rectangle(int first, int second)
+        {
+            a = first;
+            b = second;
+        }
+
+        //конструктор 3
+        public Rectangle(int side)
+        {
+            a = side;
+            b = side;
+        }
+    }
+
+    class Company
+    {
+        public string Type;
+        public string Name;
+    }
+
+    class Department
+    {
+        public Company Company;
+        public City City;
+    }
+
+    class City
+    {
+        public string Name;
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var department = GetCurrentDepartment();
+        }
+
+        static Department GetCurrentDepartment()
+        {
+            // logic
+
+        }
+    }
+
+
+}
---
+++ Module_6.3/Module_6.3/Program.cs
@@ -0,0 +1,93 @@
+﻿namespace Module_6._3
+{
+    class Company
+    {
+        public string Type;
+        public string Name;
+    }
+
+    class Department
+    {
+        public Company Company;
+        public City City;
+    }
+
+    class City
+    {
+        public string Name;
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Bus bus = new Bus();
+            bus.Load = 5;
+            bus.PrintStatus();
+    //        var department = GetCurrentDepartment();
+    //    }
+
+    //    static Department GetCurrentDepartment()
+    //    {
+    //        //Department department = new Department();
+    //        //string nameCity = department?.City?.Name;
+    //        //string typeCompany = department?.Company?.Type;
+    //        //string nameCompany = department?.Company?.Name;
+    //        //if (nameCity == "Санкт-Петербург" & typeCompany == "Банк")
+    //        //{
+    //        //   Console.WriteLine("У банка {0} есть отделение в Санкт-Петербурге", nameCompany ?? "Неизвестная компания");
+    //        //}
+    //        if (department?.Company?.Type == "Банк" && department?.City?.Name == "Санкт-Петербург")
+    //        {
+    //            Console.WriteLine("У банка {0} есть отделение в Санкт-Петербурге", department?.Company?.Name ?? "Неизвестная компания");
+    //        }
+        }
+    }
+
+    class Bus
+    {
+        public int? Load;
+
+        public void PrintStatus()
+        {
+            //Load = 10;
+            if (Load == null) Console.WriteLine("Автобус пуст");
+            else Console.WriteLine(Load);
+
+        }
+    }
+
+    class square
+    {
+        public int side;
+
+        public int S()
+        {
+            //int a = side;
+            //int b = side;
+            return side^2;
+        }
+    }
+
+    class Circle
+    {
+        public int R;
+
+        public Double S()
+        {
+            return R^2/2;
+        }
+    }
+
+    class triangle
+    {
+        public int a;
+        public int b;
+        public int c;
+
+        public Double S()
+        {
+
+        }
+    }
+}
